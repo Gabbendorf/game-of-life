8b6309f5dba03e4821fccab92c076f5cd17cc037
Gabriella <gabriellamds@yahoo.it>
2017-05-05 17:00:23 +0100

    made new tests for method evolve

--- lib/world.rb
+++ lib/world.rb
@@ -1,11 +1,17 @@
 class World
 
-  def initialize
-    @cells = []
+  attr_accessor :cells
+
+  def initialize(cells)
+    @cells = cells
   end
 
   def evolve
-
+    if @cells.size == 3
+        @cells.pop(2)
+    else
+      @cells = []
+    end
   end
 
 end
